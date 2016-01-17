7dc45cc479ba4e3e4b59486059d8bf6ee56bfec9
Ying Xiong <yxiong@dropbox.com>
2016-01-16 19:26:13 -0800

    Add LogEntry.swift

---
+++ Workout Log/LogEntry.swift
@@ -0,0 +1,19 @@
+//
+//  LogEntry.swift
+//  Workout Log
+//
+//  Created by Ying Xiong on 1/16/16.
+//  Copyright © 2016 Ying Xiong. All rights reserved.
+//
+
+import Foundation
+
+class LogEntry {
+    var name: NSString;
+    var date: NSDate;
+
+    init () {
+        name = ""
+        date = NSDate()
+    }
+}
