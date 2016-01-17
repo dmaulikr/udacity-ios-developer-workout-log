5e7f2d5e8124d10f886d2d37ec883503a6af65ed
Ying Xiong <yxiong@dropbox.com>
2016-01-16 19:32:22 -0800

    Add WorkoutLogViewController.swift

---
+++ Workout Log/WorkoutLogViewController.swift
@@ -0,0 +1,13 @@
+//
+//  WorkoutLogViewController.swift
+//  Workout Log
+//
+//  Created by Ying Xiong on 1/16/16.
+//  Copyright © 2016 Ying Xiong. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class WorkoutLogViewController: UITableViewController {
+}
