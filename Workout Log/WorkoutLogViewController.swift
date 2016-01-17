7d80a8ac4fbefabd9e0f440f92e56119c18e7c19
Ying Xiong <yxiong@dropbox.com>
2016-01-17 08:25:53 -0800

    Add entries to the main log

--- Workout Log/WorkoutLogViewController.swift
+++ Workout Log/WorkoutLogViewController.swift
@@ -10,4 +10,27 @@ import Foundation
 import UIKit
 
 class WorkoutLogViewController: UITableViewController {
+    var logEntries: [LogEntry] = [LogEntry]()
+
+    override func viewDidLoad() {
+        logEntries.append(LogEntry(dictionary: ["name": "entry1", "date": NSDate()]))
+        logEntries.append(LogEntry(dictionary: ["name": "entry2", "date": NSDate()]))
+        logEntries.append(LogEntry(dictionary: ["name": "entry3", "date": NSDate()]))
+        logEntries.append(LogEntry(dictionary: ["name": "entry4", "date": NSDate()]))
+    }
+
+    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return logEntries.count
+    }
+
+    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier("LogEntryTableCell")!
+        let entry = logEntries[indexPath.row]
+
+        cell.textLabel?.text = entry.name as String
+        let dateString = NSDateFormatter.localizedStringFromDate(entry.date, dateStyle: .MediumStyle, timeStyle: .ShortStyle)
+        cell.detailTextLabel?.text = dateString
+
+        return cell
+    }
 }
