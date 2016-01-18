bd1133a7627c2b690c34727c03befe71f0863b90
Ying Xiong <yxiong@dropbox.com>
2016-01-17 22:41:34 -0800

    Add WorkoutItemPhotoCell

---
+++ Workout Log/WorkoutItemPhotoCell.swift
@@ -0,0 +1,15 @@
+//
+//  WorkoutItemPhotoCell.swift
+//  Workout Log
+//
+//  Created by Ying Xiong on 1/17/16.
+//  Copyright © 2016 Ying Xiong. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class WorkoutItemPhotoCell: UICollectionViewCell {
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var textField: UITextField!
+}
