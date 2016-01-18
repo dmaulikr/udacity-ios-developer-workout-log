d05a1881696ccbf7f7657abf6769b4a89f046675
Ying Xiong <yxiong@dropbox.com>
2016-01-18 11:15:06 -0800

    Implement WorkoutItem

--- Workout Log/WorkoutItemViewController.swift
+++ Workout Log/WorkoutItemViewController.swift
@@ -11,4 +11,32 @@ import UIKit
 
 class WorkoutItemViewController: UICollectionViewController {
     var workoutItem: WorkoutItem?
+
+    override func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return workoutItem!.photos.count
+    }
+
+    override func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCellWithReuseIdentifier("WorkoutItemPhotoCell", forIndexPath: indexPath) as! WorkoutItemPhotoCell
+
+        let photo = workoutItem!.photos[indexPath.row]
+        if photo.image == nil {
+            cell.imageView.image = nil
+            cell.textField.hidden = false
+            cell.textField.text = "Loading..."
+            photo.getImage({() -> Void in
+                dispatch_async(dispatch_get_main_queue(), {
+                    if let c = self.collectionView!.cellForItemAtIndexPath(indexPath) as? AddEntryPhotoCell {
+                        c.textField.hidden = true
+                        c.imageView.image = photo.image
+                    }
+                })
+            })
+        } else {
+            cell.textField.hidden = true
+            cell.imageView.image = photo.image
+        }
+
+        return cell
+    }
 }
